8b9dbe2cddf65f061be1247dbd7de891648ff48d
Chad Edwards <chad@saw.dev>
2023-04-25 22:59:36 -0400

    trying again with shazam

--- video_to_playlist.py
+++ video_to_playlist.py
@@ -1,125 +1,57 @@
 import os
-import sys
-import csv
-import glob
-import logging
-from concurrent.futures import ThreadPoolExecutor
-from moviepy.editor import VideoFileClip
+import argparse
+import asyncio
+import tempfile
+from moviepy.editor import *
 from pydub import AudioSegment
-from pydub.silence import split_on_silence
 from shazamio import Shazam
 from tqdm import tqdm
 
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-def extract_audio(video_path, audio_path):
-    logger.info(f"Extracting audio from video: {video_path}")
-    clip = VideoFileClip(video_path)
-    clip.audio.write_audiofile(audio_path)
-
-
-def split_audio(audio_path, chunk_length=20000):
-    logger.info("Splitting audio into segments")
-    audio = AudioSegment.from_wav(audio_path)
-
-    segments = []
-    audio_length = len(audio)
-    for i in tqdm(range(0, audio_length, chunk_length), desc="Splitting audio", ncols=100, unit="seg"):
-        start_time = i
-        end_time = min(i + chunk_length, audio_length)
-        segment = audio[start_time:end_time]
-        segments.append(segment)
-
-    return segments
-
-
-async def identify_songs(segments):
+async def recognize_song(segment_file):
     shazam = Shazam()
-    identified_songs = []
+    song = await shazam.recognize_song(data=open(segment_file, 'rb').read())
+    return song
 
-    for idx, segment in enumerate(segments):
-        logger.info(f"Identifying song for segment {idx + 1}/{len(segments)}")
-        segment.export("temp_audio_segment.wav", format="wav")
-        result = await shazam.recognize_song("temp_audio_segment.wav")
-        matches = result.get('hits', [])
-        
-        if matches:
-            top_match = matches[0]
-            song_title = top_match['track']['title']
-            song_artist = top_match['track']['subtitle']
-            song_id = (song_title, song_artist)
-            if song_id not in identified_songs:
-                identified_songs.append(song_id)
+async def main():
+    # Step 1: Parse command line arguments
+    parser = argparse.ArgumentParser(description='Identify songs in MP4 files and create a playlist')
+    parser.add_argument('directory', type=str, help='path to the directory containing the MP4 files')
+    args = parser.parse_args()
 
-    if os.path.exists("temp_audio_segment.wav"):
-        os.remove("temp_audio_segment.wav")
+    # Step 2: Identify the MP4 files
+    mp4_files = [os.path.join(args.directory, f) for f in os.listdir(args.directory) if f.endswith('.mp4')]
 
-    return identified_songs
+    # Step 3: Extract audio from the MP4 files and split into segments
+    song_names = []
+    for mp4_file in mp4_files:
+        clip = VideoFileClip(mp4_file)
+        audio_file = mp4_file.replace('.mp4', '.mp3')
+        clip.audio.write_audiofile(audio_file)
+        sound = AudioSegment.from_mp3(audio_file)
+        duration = len(sound) / 1000
+        segment_duration = 5 * 60 * 1000  # 5 minutes in milliseconds
+        start_time = 0
+        end_time = segment_duration
+        with tqdm(total=duration, desc=f'Processing {mp4_file}') as pbar:
+            while end_time <= duration:
+                with tempfile.NamedTemporaryFile(suffix='.mp3') as tmp_file:
+                    segment = sound[start_time:end_time]
+                    segment.export(tmp_file.name, format='mp3')
+                    song = await recognize_song(tmp_file.name)
+                    if song:
+                        song_names.append(song['track']['title'])
+                    start_time += segment_duration
+                    end_time += segment_duration
+                    pbar.update(segment_duration / 1000)
 
-
-
-def generate_csv(identified_songs, output_csv):
-    logger.info(f"Generating CSV playlist: {output_csv}")
-    with open(output_csv, mode='w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['Title', 'Artist']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for song in identified_songs:
-            writer.writerow({'Title': song[0], 'Artist': song[1]})
-
-def process_video(video_path, output_csv):
-    logger.info(f"Processing video: {video_path}")
-
-    audio_path = "temp_audio_file.wav"
-    
-    # Ensure the directory is writable
-    audio_dir = os.path.dirname(audio_path)
-    if not os.access(audio_dir, os.W_OK):
-        logger.error(f"The directory '{audio_dir}' is not writable. Please check the permissions and try again.")
-        return
-
-    extract_audio(video_path, audio_path)
-    segments = split_audio(audio_path)
-    identified_songs = await identify_songs(segments)
-    generate_csv(identified_songs, output_csv)
-
-    if os.path.exists(audio_path):
-        os.remove(audio_path)
-
-    logger.info(f"Finished processing video: {video_path}")
-
-
-def find_mp4_files(folder_path):
-    mp4_files = glob.glob(os.path.join(folder_path, "*.mp4"))
-    return mp4_files
-
-async def main(video_paths):
-    with ThreadPoolExecutor() as executor:
-        futures = []
-        for video_path in video_paths:
-            output_csv = os.path.splitext(video_path)[0] + ".csv"
-            future = executor.submit(asyncio.run, process_video(video_path, output_csv))
-            futures.append(future)
-
-        for future in futures:
-            future.result()
-
-if __name__ == "__main__":
-    import asyncio
-
-    if len(sys.argv) < 2:
-        logger.info("Usage: python video_to_playlist.py [path/to/your/folder]")
-        logger.info("No folder path provided, using default folder: /app/data")
-        folder_path = "/app/data"
+    # Step 4: Create the playlist
+    if song_names:
+        with open('playlist.csv', 'w') as f:
+            for song_name in song_names:
+                f.write(f'{song_name}\n')
+        print(f'Successfully created playlist with {len(song_names)} songs')
     else:
-        folder_path = sys.argv[1]
+        print('No songs found')
 
-    video_paths = find_mp4_files(folder_path)
-    if not video_paths:
-        logger.error(f"No MP4 files found in folder: {folder_path}")
-    else:
-        logger.info(f"Found {len(video_paths)} MP4 files in folder: {folder_path}")
-        asyncio.run(main(video_paths))
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
